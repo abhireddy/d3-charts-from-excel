9958006f6564887a431ce7f5cd90a5227013865b
Abhi Reddy <abhi.reddy@gmail.com>
2016-08-06 09:59:31 -0500

    hard auth

--- scripts/createcsv.php
+++ scripts/createcsv.php
@@ -15,8 +15,18 @@
 	require('../vendor/autoload.php');
 
 	// this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
+	/*
 	$s3 = Aws\S3\S3Client::factory();
 	$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
+	*/
+
+	$s3 = Aws\S3\S3Client::factory(array(
+    	'key' => 'AKIAJRSJS5K26A4IH4FQ',
+    	'secret' => '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'
+    ));
+
+	$bucket = "snapchart";
+
     $upload = $s3->upload($bucket, $filename, fopen('../'.$filename, 'rb'), 'public-read');
 
 	//create DB record
