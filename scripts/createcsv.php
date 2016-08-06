1a9f13f2e3f760e7996b6a1876f14ddce6d2a0e9
Abhi Reddy <abhi.reddy@gmail.com>
2016-08-06 01:45:03 -0500

    testing

--- scripts/createcsv.php
+++ scripts/createcsv.php
@@ -12,7 +12,7 @@
 	fclose($fp);
 
 	//upload CSV to Amazon S3 bucket
-	//require('vendor/autoload.php');
+	require('vendor/autoload.php');
 
 	// this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
 	//$s3 = Aws\S3\S3Client::factory();
