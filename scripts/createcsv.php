52ae22bad667ab18a5cca5ca21ce1fa7591047e3
Abhi Reddy <abhi.reddy@gmail.com>
2016-08-06 01:50:04 -0500

    testing

--- scripts/createcsv.php
+++ scripts/createcsv.php
@@ -12,12 +12,12 @@
 	fclose($fp);
 
 	//upload CSV to Amazon S3 bucket
-	require('vendor/autoload.php');
+	require('../vendor/autoload.php');
 
 	// this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
-	//$s3 = Aws\S3\S3Client::factory();
-	//$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
-    //$upload = $s3->upload($bucket, $filename, fopen('../'.$filename, 'rb'), 'public-read');
+	$s3 = Aws\S3\S3Client::factory();
+	$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
+    $upload = $s3->upload($bucket, $filename, fopen('../'.$filename, 'rb'), 'public-read');
 
 	//create DB record
 	$link = mysqli_connect('us-cdbr-iron-east-03.cleardb.net', 'b253f1008c6fa9', '3755331b', 'heroku_d54ce66a7bbf72b'); //, 'ebdb', 3306);
