660ddf96134c0cec4ef0de840f6494cb2f7489c2
Abhi Reddy <abhi.reddy@gmail.com>
2016-08-06 13:07:27 -0500

    fixxed?

--- scripts/createcsv.php
+++ scripts/createcsv.php
@@ -15,20 +15,20 @@
 	require('../vendor/autoload.php');
 
 	// this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
-//	$s3 = Aws\S3\S3Client::factory();
-//	$bucket = getenv('S3_BUCKET_NAME')?: die('No "S3_BUCKET_NAME" config var in found in env!');
+	$s3 = Aws\S3\S3Client::factory();
+	$bucket = getenv('S3_BUCKET_NAME')?: die('No "S3_BUCKET_NAME" config var in found in env!');
 
-
+/*
 	$s3 = Aws\S3\S3Client::factory(array(
     	'key' => 'AKIAJRSJS5K26A4IH4FQ',
     	'secret' => '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'
     ));
     $bucket = "snapchart";
-	
+*/	
     $upload = $s3->upload($bucket, $filename, fopen('../'.$filename, 'rb'), 'public-read');
 
 	//create DB record
-	$link = mysqli_connect('us-cdbr-iron-east-03.cleardb.net', 'b253f1008c6fa9', '3755331b', 'heroku_d54ce66a7bbf72b'); //, 'ebdb', 3306);
+	$link = mysqli_connect('us-cdbr-iron-east-03.cleardb.net', 'b253f1008c6fa9', '3755331b', 'heroku_d54ce66a7bbf72b');
 	$stmt = mysqli_prepare($link, "INSERT INTO charts (filename, charttype, expiration) VALUES (?, ?, ?)");
 	mysqli_stmt_bind_param($stmt, 'sss', $filename, $chart, $expiration);
 	//execute statement
